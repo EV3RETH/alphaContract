8132dfa66c8929744799363065bdeb56c0813d71
Scott <seharlan@gmail.com>
2021-05-18 21:43:10 -0700

    message board

--- packages/react-app/src/E-Pages/Homepage/MessageBoard/MessageBoard.jsx
+++ packages/react-app/src/E-Pages/Homepage/MessageBoard/MessageBoard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { JsonRpcProvider } from "@ethersproject/providers";
 import './MessageBoard.css'
 import { useContractLoader, useContractReader } from '../../../hooks'
@@ -16,10 +16,16 @@ const localProviderUrlFromEnv = process.env.REACT_APP_PROVIDER ? process.env.REA
 const localProvider = new JsonRpcProvider(localProviderUrlFromEnv);
 
 export default function MessageBoard() {
+	const [open, setOpen] = useState(false)
 
 	const readContracts = useContractLoader(localProvider)
-	const winner = useContractReader(readContracts, "AlphaTokens", "lottoWinner")
+	const winner = useContractReader(readContracts, "AlphaTokens", "whoGetsTheX")
+	const remaining = parseInt(useContractReader(readContracts, "AlphaTokens", "remaining")) - 13 || 10
+	const pool = useContractReader(readContracts, "AlphaTokens", "addressPool")
 
+	function togglePool() {
+		setOpen(prev => !prev)
+	}
 	return (
 		<div className="message-board">
 			<div className="message-container">
@@ -35,41 +41,49 @@ export default function MessageBoard() {
 					<h1>Alpha Tokens</h1>
 					<p>
 						My genisis set of Non-Fungible Tokens (NFTs).
-						While most of my future works will be audio based, owning one of these tokens proves cryptocraphically that you are indisputedly an original supporter of mine.
+						Owning one of these tokens proves thoguth cryptography that you are indisputedly an original supporter of mine.
 						For that let me give you my deepest thanks in advance <span role="img">❤️</span>
 					</p>
 					<h2>What else makes them special?</h2>
 					<p>
 						Every single one of these tokens are unique and will never be minted again.
-						The tokens will live forever on the Ethereum Mainnet and the image assets themselves are hosted on the disributed web via <a href="https://ipfs.io/" target="_blank" rel="noopener noreferrer">IPFS</a> so unlike most tokens yours will be be truly and completely decentralized.
+						The tokens will live forever on the Ethereum Mainnet and the image assets themselves are hosted on the disributed web via <a href="https://ipfs.io/" target="_blank" rel="noopener noreferrer">IPFS</a> so unlike most NFTs yours will be be truly and completely decentralized.
 						Alpha Token's smart contract also extends the ERC721 token contract to include additional perks for Alpha Token holders.
 					</p>
 					<h2>More perks you say? Tell me more!</h2>
 					<p>
-						Every time an Alpha Token is traded the new holder's wallet address is stored on chain in the contract's "lottery pool".
-						After the 11th trade a function within the contract will automatically send the "Y" token to a random winner chosen from the pool.
-						To accomidate memory concerns (which is very expensive on-chain) the pool will be cleared after this.
-						After the 22nd trade the "Z" token will be released.
+						Every time an Alpha Token is sold or traded the new holder's wallet address is stored on chain in the contract's "address pool".
+						After 10 tokens have been sold AlphaTokens smart contract (which can be verified <a>here</a>) will automaticall choose a winner from this pool and send them the "X" token. After this happens the pool will close.
 					</p>
-					<i>It is the 11th trade and not the 11th token sold, so if the "A" token trades hands 11 times the lottery winner will be chosen. There is however a maximum of 1 entry per wallet address</i>
 					<h2>What varieties are there?</h2>
 					<p>
-						There are three tiers of token: bronze, silver, and gold.
+						There are three tiers of AlphaTokens: Bronze, Silver, and Gold.
 						Bronze and Silver starting bids will be priced accordingly.
-						The gold status is considered the most rare and not for sale initially, although they may be sold after they are given away.
-						These are reserved as for lottery winnings, social media incentives, and the "E" token which I will be holding onto myself for the forseable future <span role="img">🤷</span>
+						The Gold status is considered the most rare and not for sale initially, although they may be sold after they are given away.
+						The "X" will go to a winner chosen randomly by the contract, "Y" and "Z" will be given away randomly by me as encentives for spreading the word on social media, and I will be holding onto the "E" token myself for the forseable future <span role="img">🤷</span>
 					</p>
 					<i>
 						Note: in addition to OpenSea's 2.5% seller fee, I have implimented a 7.5% royalty sellers fee that will go to me if you were to sell your token.
-						So when you make your millions selling these tokens down the line you will still be supporting me. Thank you!
+						So when you make your millions selling these tokens down the line you will still be supporting me <span role="img">😁</span> Thank you!
 					</i>
 				</div>
 				<div className="winner">
-					<h1>Lottery</h1>
-					<button>Click here to see whos in the lottery so far</button>
-					<p><i>The winner of the lottery is</i></p>
-					<strong>{winner ? winner : "Not been selected yet"}</strong>
+					<h1>Who gets the X?</h1>
+					{!winner ? <i>Be one of the first ten AlphaToken buyers and it could be you!</i> : null}
+					<button onClick={togglePool}>
+						Click here to see the current address pool
+					</button>
+					<div className={`pool${ open ? " open" : "" }`}>
+						<button onClick={togglePool}>X</button>
+						<p>{pool}</p>
+					</div>
+
+					{winner
+						? <strong><i>{winner}</i> is the winning address, they get the X. <br />Congratulations!</strong>
+						: <p><strong>After <i>~ {remaining} ~</i> more Alpha Tokens are sold the address pool closes forever!</strong>The winner will automatically be sent the X token and their address will be displayed here *</p>
+					}
 				</div>
+				<i className="disclaimer">* if you do not want your public address displayed please contect me and I will remove it from the website.</i>
 			</div>
 		</div>
 	)
